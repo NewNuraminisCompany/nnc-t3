788e0f0402283e7f9fec1b2ce861698ab5db65f9
ascariis <albertoporcu4@gmail.com>
2024-08-02 10:59:11 +0200

    Modifica e eliminazione squadre

--- src/components/actions.ts
+++ src/components/actions.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import { Squadre } from "@/app/(dash-lyt)/dashboard/iscrizioni/columns";
 import { db } from "@/server/db"; // Import your Drizzle ORM setup
 import { tornei, squadre, giocatori } from "@/server/db/schema"; // Import the schema
 
@@ -9,11 +10,15 @@ export async function getTornei() {
 }
 
 import { createId } from '@paralleldrive/cuid2';
+import { eq } from "drizzle-orm";
 
 type TeamData = {
+  idSquadra: string;
   nome: string;
   colore: string;
   cellulare: string;
+  statoAccettazione: boolean;
+  idTorneo: string;
 };
 
 type PlayerData = {
@@ -23,6 +28,34 @@ type PlayerData = {
   dataNascita: string;
 };
 
+export async function updateSquadra(squadra: Squadre) {
+  try {
+    await db.update(squadre)
+      .set({
+        nome: squadra.nome,
+        colore: squadra.colore,
+        cellulare: squadra.cellulare,
+        statoAccettazione: squadra.statoAccettazione,
+      })
+      .where(eq(squadre.idSquadra, squadra.idSquadra));
+    return { success: true };
+  } catch (error) {
+    console.error("Error in updateSquadra:", error);
+    return { success: false, error: "Failed to update squadra" };
+  }
+}
+
+export async function deleteSquadra(squadra: Squadre) {
+  try {
+    await db.delete(squadre).where(eq(squadre.idSquadra, squadra.idSquadra));
+    return { success: true };
+  } catch (error) {
+    console.error("Error in deleteSquadra:", error);
+    return { success: false, error: "Failed to delete squadra" };
+  }
+}
+
+
 export async function submitTeamAndPlayers({ team, players }: { team: TeamData, players: PlayerData[] }) {
   try {
     const [insertedTeam] = await db.insert(squadre).values({
@@ -30,6 +63,7 @@ export async function submitTeamAndPlayers({ team, players }: { team: TeamData, 
       nome: team.nome,
       colore: team.colore,
       cellulare: team.cellulare,
+      idTorneo: team.idTorneo, // Ensure to include this field
       statoAccettazione: false,
     }).returning();
 
