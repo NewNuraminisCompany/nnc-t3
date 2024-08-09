28bc94817a8505e4188a9fda87ef1418bc4b5c68
daviicocco <99872428+daviicocco@users.noreply.github.com>
2024-08-09 19:30:00 +0200

    BUILDAAAA

--- src/app/(user-lyt)/iscrizioni/page.tsx
+++ src/app/(user-lyt)/iscrizioni/page.tsx
@@ -1,11 +1,15 @@
-import IscrizioniSquadre from "@/components/IscrizioniSquadre"
+import { Suspense } from 'react'
+import dynamic from 'next/dynamic'
+
+const IscrizioniSquadre = dynamic(() => import("@/components/IscrizioniSquadre"), { ssr: false })
 
 export default function Iscrizioni() {
   return (
     <div className="flex flex-col w-full ">
       <h1 className="text-4xl font-bold my-4">Iscrizioni</h1>
-      <IscrizioniSquadre />
+      <Suspense fallback={<div>Loading...</div>}>
+        <IscrizioniSquadre />
+      </Suspense>
     </div>
   )
 }
-
