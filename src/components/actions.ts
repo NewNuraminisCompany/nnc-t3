03700efe0c80686202871247aa8defd5f491e3bb
RubenLasio <rubenlasio05@gmail.com>
2024-08-10 10:37:09 +0200

    corretto

--- src/components/actions.ts
+++ src/components/actions.ts
@@ -3,23 +3,13 @@
 import { db } from "@/server/db"; // Import your Drizzle ORM setup
 import { avvenimenti, gap, giocatori, partite, squadre, tornei } from "@/server/db/schema"; // Import the schema
 import type {
-<<<<<<< HEAD
   EditPlayerData,
   EditTeamData,
-  EditTorneoData,
   PlayerData,
   TeamData,
   TorneoData,
   PartitaData,
-  AvvenimentoData,
-=======
-    EditPlayerData,
-    EditTeamData,
-    PartitaData,
-    PlayerData,
-    TeamData,
-    TorneoData
->>>>>>> 4a803212ece7fe8246a95c2abf16c687f9fcdb64
+  AvvenimentoData
 } from "@/types/db-types";
 export async function getTornei() {
   const result = await db.select().from(tornei);
@@ -415,46 +405,6 @@ export async function fetchPartite(idTorn: string) {
     console.error("Error fetching partite:", error);
     throw new Error(
       `Failed to fetch partite: ${error instanceof Error ? error.message : String(error)}`,
-    );
-  }
-}
-
-export async function fetchPartite2(idTorn : string) {
-  try {
-    console.log("Attempting to fetch data from partite table...");
-    console.log("id torneo: ",idTorn);
-    const result = await db
-    .select({
-      idPartita: partite.idPartita,
-      idSquadra1: partite.idSquadra1,
-      idSquadra2: partite.idSquadra2,
-      risultatoSquadra1: partite.risultatoSquadra1,
-      risultatoSquadra2: partite.risultatoSquadra2,
-      dataOra: partite.dataOra,
-      girone: partite.girone
-    } )
-    .from(partite)
-    .innerJoin(squadre, eq(partite.idSquadra1, squadre.idSquadra))
-    .where(eq(squadre.idTorneo,idTorn));
-
-    if (result.length === 0) {
-      console.log("No records found in the partite table.");
-      return [];
-    }
-
-    return result.map((partite) => ({
-      idPartita: partite.idPartita,
-      idSquadra1: partite.idSquadra1,
-      idSquadra2: partite.idSquadra2,
-      risultatoSquadra1: partite.risultatoSquadra1,
-      risultatoSquadra2: partite.risultatoSquadra2,
-      dataOra: new Date(partite.dataOra),
-      girone: partite.girone,
-    }));
-  } catch (error) {
-    console.error("Error fetching partite:", error);
-    throw new Error(
-      Failed to fetch partite: ${error instanceof Error ? error.message : String(error)},
     );
   }
 }
